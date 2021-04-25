55fa83299d43f807e7f7b636117852a658dc7803
Alhajras Algdairy <alhajras.algdairy@gmail.com>
2021-04-25 04:18:43 +0200

    add vec3 to main

---
+++ Raytracer/Raytracer.cpp
@@ -0,0 +1,49 @@
+// Raytracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include "vec3.h"
+
+using namespace std;
+
+int main()
+{
+	ofstream myfile;
+	myfile.open("image.ppm");
+
+	int nx = 200; // number of columns width
+	int ny = 100; // number of rows hight
+	myfile << "P3\n" << nx << " " << ny << "\n255\n";
+
+	for (int j = ny-1; j>=0;j--){
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+
+			vec3 col(r, b, g);
+
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+
+			myfile << ir << " " << ig << " " << ib << "\n";
+		}
+	
+	}
+	myfile.close();
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
